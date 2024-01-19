9a2fa2787901aeee846af21368ec6ff409c4295c
MisahSaid <misah.said.said@hva.nl>
2024-01-19 10:04:37 +0100

    h3 --> h4

---
+++ script/script.js
@@ -0,0 +1,7 @@
+// In JavaScript
+// Bedenk een naam die nog niet in gebruik is en declareer die met let;
+// Selecteer met JavaScript het juiste element uit de DOM met behulp van document.querySelector();
+// Gebruik in JavaScript naam.addEventListener() om een event aan een element te koppelen;
+// Gebruik in JavaScript naam.classList.toggle() om een andere class aan een element toe te voegen.
+// ProTip: Gebruik bij elke stap console.log() om te controleren of alles werkt zoals je verwacht dat het werkt.
+
