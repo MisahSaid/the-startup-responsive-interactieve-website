c9c22084226ff8d950cb7762405506d8f355b02f
MisahSaid <misah.said.said@hva.nl>
2024-01-19 10:28:20 +0100

    form

--- script/script.js
+++ script/script.js
@@ -5,3 +5,14 @@
 // Gebruik in JavaScript naam.classList.toggle() om een andere class aan een element toe te voegen.
 // ProTip: Gebruik bij elke stap console.log() om te controleren of alles werkt zoals je verwacht dat het werkt.
 
+
+// classes aanroepen van de HTML en in variabele stoppen
+const buttonAnimationClick = document.querySelectorAll(".be-there");
+const squareAnimation = document.querySelectorAll(".square");
+
+// Eventhandler op variabele wanneer er op die class op "geclicked"
+buttonAnimationClick.addEventListener("click", () => {
+// Als er wordt geclicked dan wordt deze pagetransition van
+// geactiveerd door dat de class wordt toegevoegd op de square
+squareAnimation.classList.add(".pagetransition")
+});
